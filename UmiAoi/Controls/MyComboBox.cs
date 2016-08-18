0f7ca494369d5c3f45b1f7986f5f504ffc84b311
aoi-umi <1641622365@qq.com>
2016-08-18 11:35:14 +0800

    MyComboBox .3 first finished

--- UmiAoi/Controls/MyComboBox.cs
+++ UmiAoi/Controls/MyComboBox.cs
@@ -1,29 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace UmiAoi.Controls
 {
     /// <summary>
-    /// Interaction logic for MyComboBox.xaml
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:UmiAoi"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:UmiAoi;assembly=UmiAoi"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:MyComboBox/>
+    ///
     /// </summary>
-    public partial class MyComboBox : ComboBox
+    public class MyComboBox : ComboBox
     {
-        public MyComboBox()
+        static MyComboBox()
         {
-            InitializeComponent();
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyComboBox), new FrameworkPropertyMetadata(typeof(MyComboBox)));
+        }
+
+        public MyComboBox() : base()
+        {
         }
 
         public override void OnApplyTemplate()
@@ -32,13 +50,12 @@ namespace UmiAoi.Controls
             Update();
         }
 
-
         protected override void OnSelectionChanged(SelectionChangedEventArgs e)
         {
             base.OnSelectionChanged(e);
             Update();
         }
-
+        
         private void Update()
         {
             if (SelectedIndex >= 0 && SelectedIndex < Items.Count)
@@ -63,7 +80,7 @@ namespace UmiAoi.Controls
                 SelectedItemHeader = string.Empty;
                 MySelectedItem = null;
             }
-        }        
+        }
 
         public string SelectedItemHeader
         {
@@ -75,7 +92,7 @@ namespace UmiAoi.Controls
         public static readonly DependencyProperty SelectedItemHeaderProperty =
             DependencyProperty.Register("SelectedItemHeader", typeof(string), typeof(MyComboBox), new PropertyMetadata(string.Empty));
 
-        public Object MySelectedItem    
+        public Object MySelectedItem
         {
             get { return (Object)GetValue(MySelectedItemProperty); }
             set { SetValue(MySelectedItemProperty, value); }
@@ -85,6 +102,15 @@ namespace UmiAoi.Controls
         public static readonly DependencyProperty MySelectedItemProperty =
             DependencyProperty.Register("MySelectedItem", typeof(Object), typeof(MyComboBox), new PropertyMetadata(null));
 
+        public Dock ComboBoxPlacement
+        {
+            get { return (Dock)GetValue(ComboBoxPlacementProperty); }
+            set { SetValue(ComboBoxPlacementProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ComboBoxPlacementProperty =
+            DependencyProperty.Register("ComboBoxPlacement", typeof(Dock), typeof(MyComboBox), new PropertyMetadata(null));
 
 
     }
