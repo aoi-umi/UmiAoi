dc788be7bb13a2aa1d2fd58445f0a5445a07461a
aoi-umi <1641622365@qq.com>
2016-09-07 10:48:36 +0800

    +Helper  BindingHelper

---
+++ UmiAoi/Helper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UmiAoi
+{
+    public static class Helper
+    {
+        public static void BindingHelper(BindingModel bindingModel)
+        {
+            if (bindingModel.BindingElement == null) throw new NullReferenceException(nameof(bindingModel.BindingElement));
+            var binding = new Binding();
+            if (bindingModel.Source != null) binding.Source = bindingModel.Source;
+            if (bindingModel.Path != null) binding.Path = new PropertyPath(bindingModel.Path);
+            if (bindingModel.ElementName != null) binding.ElementName = bindingModel.ElementName;
+            binding.Mode = bindingModel.BindingMode;
+            bindingModel.BindingElement.SetBinding(bindingModel.Property, binding);
+        }
+    }
+
+    public class BindingModel
+    {
+        public FrameworkElement BindingElement { get; set; }
+        public DependencyProperty Property { get; set; }
+        public Object Source { get; set; }
+        public string Path { get; set; }
+        public BindingMode BindingMode { get; set; }
+        public String ElementName { get; set; }
+    }
+}
